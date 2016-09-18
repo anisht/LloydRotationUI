d9d0b9ec8d228021117191143affe9e5f29e23be
hatooku <jhlee@caltech.edu>
2016-09-18 15:47:48 -0700

    Implement react-router-restore-scroll

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, hashHistory, applyRouterMiddleware } from 'react-router'
+import { useHistoryRestoreScroll,useRouterRestoreScroll } from 'react-router-restore-scroll'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import {amber500, amber700} from 'material-ui/styles/colors';
@@ -16,13 +17,21 @@ const muiTheme = getMuiTheme({
   },
 });
 
+// react-router-restore-scroll setup
+const createHistory = useHistoryRestoreScroll(() => hashHistory);
+const routerRender = applyRouterMiddleware(
+  useRouterRestoreScroll()
+);
 
 const routes = (
     <MuiThemeProvider muiTheme={muiTheme}>
-        <Router history={hashHistory}>
+        <Router
+            history={createHistory()}
+            render={routerRender}
+        >
             <Route path='/' component={Main}>
                 <IndexRoute component={Home} />
-                <Route path='/frosh/:id' component={FroshPageContainer} />
+                <Route path='/frosh/:prefrosh_id' component={FroshPageContainer} />
             </Route>
         </Router>
     </MuiThemeProvider>
