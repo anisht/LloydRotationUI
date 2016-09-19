a72ba54b58d1368ff5bf9ea8c97aac572409f779
hatooku <jhlee@caltech.edu>
2016-09-18 17:50:54 -0700

    Change right side of AppBar to only logout button

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,30 +1,14 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
-import IconMenu from 'material-ui/IconMenu';
-import MenuItem from 'material-ui/MenuItem';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-import { grey50 } from 'material-ui/styles/colors';
+import FlatButton from 'material-ui/FlatButton';
+import { pinkA200 } from 'material-ui/styles/colors';
 
 const styles = {
   title: {
     cursor: 'pointer',
   },
 };
-
-const RightMenu = (props) => (
-  <IconMenu
-    iconButtonElement={
-        
-      <IconButton><MoreVertIcon color={grey50}/></IconButton>
-    }
-    targetOrigin={{horizontal: 'right', vertical: 'top'}}
-    anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-  >
-    <MenuItem primaryText="Search" />
-    <MenuItem primaryText="Logout" href="../lloyd/logout"/>
-  </IconMenu>
-)
 
 const Header = (props) => (
     <AppBar
@@ -35,7 +19,7 @@ const Header = (props) => (
                 <img width="24px" src="https://s10.postimg.org/3thles6ll/0d35c09fc1edc33142925224e9029e10.png"/>
             </IconButton>
         }
-        iconElementRight={<RightMenu />}
+        iconElementRight={<FlatButton label="Logout" href="../lloyd/logout" />}
     />
 );
 
