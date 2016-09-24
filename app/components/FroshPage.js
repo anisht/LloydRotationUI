54e9082e10937eaedf49622b47ad2bf545bba9e9
hatooku <jhlee@caltech.edu>
2016-09-23 18:57:13 -0700

    Change page title name to Frosh name for FroshPage

--- app/components/FroshPage.js
+++ app/components/FroshPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Avatar from 'material-ui/Avatar';
 import {Grid, Row, Col} from 'react-flexbox-grid';
+import DocumentTitle from 'react-document-title';
 
 import FroshDetailedCard from './FroshDetailedCard';
 import CommentContainer from '../containers/CommentContainer';
@@ -9,36 +10,38 @@ import CommentFormContainer from '../containers/CommentFormContainer';
 import RatingFormContainer from '../containers/RatingFormContainer';
 
 const FroshPage = (props) => (
-    <Grid>
-        <Row>
-            <Col xs={12} sm={4} md={4}>
-                <FroshDetailedCard frosh={props.frosh}/>
-            </Col>
-            <Col xs={12} sm={8} md={8}>
-                <div style={{margin: "40px 20px 20px 20px"}}>
-                    <RatingFormContainer
-                        currentRating={props.currentRating}
-                        updateRating={props.updateRating}
-                    />
-                </div>
-                <div style={{margin: "20px"}}>
-                    <CommentFormContainer addComment={props.addComment}/>
-                    <br/>
-                    {props.comments.map(function(comment, index){
-                        return (
-                            <CommentContainer
-                                key={index}
-                                author={comment.user}
-                                content={comment.content}
-                                comment_id={comment.id}
-                                deleteComment={props.deleteComment}
-                            />
-                        )
-                    })}
-                </div>
-            </Col>
-        </Row>
-    </Grid>
+    <DocumentTitle title={props.frosh.displayName}>
+        <Grid>
+            <Row>
+                <Col xs={12} sm={4} md={4}>
+                    <FroshDetailedCard frosh={props.frosh}/>
+                </Col>
+                <Col xs={12} sm={8} md={8}>
+                    <div style={{margin: "40px 20px 20px 20px"}}>
+                        <RatingFormContainer
+                            currentRating={props.currentRating}
+                            updateRating={props.updateRating}
+                        />
+                    </div>
+                    <div style={{margin: "20px"}}>
+                        <CommentFormContainer addComment={props.addComment}/>
+                        <br/>
+                        {props.comments.map(function(comment, index){
+                            return (
+                                <CommentContainer
+                                    key={index}
+                                    author={comment.user}
+                                    content={comment.content}
+                                    comment_id={comment.id}
+                                    deleteComment={props.deleteComment}
+                                />
+                            )
+                        })}
+                    </div>
+                </Col>
+            </Row>
+        </Grid>
+    </DocumentTitle>
 );
 
 export default FroshPage;
